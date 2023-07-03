0d5fe14d036e8ef30530075b7b8cd067c5ec4fad
MakhachB <105069099+MakhachB@users.noreply.github.com>
2023-07-03 22:44:12 +0300

    2.2: chapter 1.3 - 2.2

--- ch01/taco-cloud/src/main/java/sia/tacocloud/controller/HomeController.java
+++ ch01/taco-cloud/src/main/java/sia/tacocloud/controller/HomeController.java
@@ -1,13 +1,22 @@
 package sia.tacocloud.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
+        return "home";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
         return "home";
     }
 }
