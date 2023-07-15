d88ea9989ea580cbd5ee15234686ae682ff02461
MakhachB <105069099+MakhachB@users.noreply.github.com>
2023-07-15 22:30:26 +0300

    7.1: rest

---
+++ ch07/taco-cloud7-rest/src/main/java/com/tacocloud7rest/TacoCloud7RestApplication.java
@@ -0,0 +1,13 @@
+package com.tacocloud7rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloud7RestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloud7RestApplication.class, args);
+    }
+
+}
