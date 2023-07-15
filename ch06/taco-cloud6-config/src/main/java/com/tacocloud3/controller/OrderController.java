1e64d5bfd7cb337598580db6a82069edd0482bf0
MakhachB <105069099+MakhachB@users.noreply.github.com>
2023-07-15 22:24:16 +0300

    7.1: rest

--- ch06/taco-cloud6-config/src/main/java/com/tacocloud3/controller/OrderController.java
+++ ch06/taco-cloud6-config/src/main/java/com/tacocloud3/controller/OrderController.java
@@ -15,10 +15,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
